ae636554e4db9a2905d7df27fce61cd847ada5e8
LENSPIRA <lenspira@outlook.com>
2021-08-26 20:32:28 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+  VoiceRSS.speech({
+    key: "API_KEY_HERE",
+    src: joke,
+    hl: "en-us",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+
+// Get Jokes from Joke API
+async function getJokes() {
+  let joke = "";
+  const apiUrl = "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist";
+  try {
+    // Disable Button
+    toggleButton();
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Text-to-Speech
+    tellMe(joke);
+  } catch (error) {
+    // Catch errors here
+    console.log("oops", error);
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
